f5021912547294168f1d8e8febef160cdc9442b5
tpires1041 <tiagopires2501@outlook.com>
2024-01-31 00:36:50 -0400

    Outro teste com documentação
    Testando os trechos do site do AdminJS

--- admin.js
+++ admin.js
@@ -1,6 +1,6 @@
 require('dotenv/config');
 
-import { ComponentLoader } from 'adminjs'
+
 const AdminJS = require('adminjs');
 const AdminJSExpress = require('@adminjs/express');
 const AdminJSSequelize = require('@adminjs/sequelize')
@@ -8,6 +8,8 @@ const options = require('./config/options.js');
 AdminJS.registerAdapter(AdminJSSequelize);
 const { Usuario } = require('./app/models');
 const bcrypt = require('bcryptjs');
+import { ComponentLoader } from 'adminjs'
+
 const componentLoader = new ComponentLoader()
 
 const Components = {
@@ -15,31 +17,11 @@ const Components = {
   // other custom components
 }
 
-const options = {
+const admin = new AdminJS({
   dashboard: {
     component: Components.Dashboard,
   },
   componentLoader
-}
-
+})
 
 const adminJs = new AdminJS(options)
-
-if (process.env.NODE_ENV == 'production'){
-  module.exports = adminRouter = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
-    authenticate: async (email, password) => {
-      const usuario = await Usuario.findOne({ email })
-      if (usuario) {
-        const matched = await bcrypt.compare(password, usuario.password)
-        if (matched) {
-          return usuario
-        }
-      }
-      return false
-    },
-    cookieName: process.env.COOKIE_NAME,
-    cookiePassword: process.env.COOKIE_PASSWORD,
-  }, null, { resave: false, saveUninitialized: true });
-} else {
-  module.exports = adminRouter = AdminJSExpress.buildRouter(adminJs);
-}
