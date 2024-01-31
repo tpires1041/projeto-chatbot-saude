16d78e3b5e74e7b0229818f051ad82ae2c7ae6fb
tpires1041 <tiagopires2501@outlook.com>
2024-01-31 00:41:09 -0400

    Mais mudanças
    Provavelmente agora essa propaganda maluca some

--- admin.js
+++ admin.js
@@ -4,6 +4,7 @@ require('dotenv/config');
 const AdminJS = require('adminjs');
 const AdminJSExpress = require('@adminjs/express');
 const AdminJSSequelize = require('@adminjs/sequelize')
+import { ComponentLoader } from 'adminjs'
 const options = require('./config/options.js');
 AdminJS.registerAdapter(AdminJSSequelize);
 const { Usuario } = require('./app/models');
@@ -17,11 +18,23 @@ const Components = {
   // other custom components
 }
 
-const admin = new AdminJS({
-  dashboard: {
-    component: Components.Dashboard,
-  },
-  componentLoader
-})
-
 const adminJs = new AdminJS(options)
+
+if (process.env.NODE_ENV == 'production'){
+  module.exports = adminRouter = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
+    authenticate: async (email, password) => {
+      const usuario = await Usuario.findOne({ email })
+      if (usuario) {
+        const matched = await bcrypt.compare(password, usuario.password)
+        if (matched) {
+          return usuario
+        }
+      }
+      return false
+    },
+    cookieName: process.env.COOKIE_NAME,
+    cookiePassword: process.env.COOKIE_PASSWORD,
+  }, null, { resave: false, saveUninitialized: true });
+} else {
+  module.exports = adminRouter = AdminJSExpress.buildRouter(adminJs);
+}
